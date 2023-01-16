72e7fe688e407a780f9b374240f60035a11fb531
Kimajswl <ajswl4763@naver.com>
2023-01-16 17:09:40 +0900

    first commit

---
+++ IdeaProjects/demo2/src/main/java/com/example/demo/account/SignUpForm.java
@@ -0,0 +1,12 @@
+package com.example.demo.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
